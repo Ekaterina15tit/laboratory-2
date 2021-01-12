9e3683b030bcf24ab60a61484ab8f84e7e150155
Ekaterina15tit <76525883+Ekaterina15tit@users.noreply.github.com>
2021-01-12 21:44:10 +0500

    laboratory 2

---
+++ Лабораторная 7/Лабораторная 7/Лабораторная 7.cpp
@@ -0,0 +1,46 @@
+﻿#include <stdio.h>
+#include <Windows.h> 
+#include <iostream>
+
+using namespace std;
+
+void Add(float x, float y)
+{
+    cout << "Функция для выситания целых чисел (float)" << endl;
+    cout << x << " - " << y << " = " << (x - y) << endl;
+
+}
+
+void Add(double x, double y)
+{
+    cout << "Функция для вычитания целых чисел(double)" << endl;
+    cout << x << " - " << y << " = " << (x - y) << endl;
+}
+
+void Add(float x, float xi, float y, float yi)
+{
+    cout << "Функция для вычитания комплексных чисел(float)" << endl;
+    cout << "(" << x << " - " << xi << "i) - (" << y << " - " << yi << "i) = " << x - y << " - " << xi - yi << "i" << endl;
+
+}
+
+void Add(double x, double xi, double y, double yi)
+{
+    cout << "Функция для вычитания комплексных чисел(double)" << endl;
+    cout << "(" << x << " - " << xi << "i) - (" << y << " - " << yi << "i) = " << x - y << " - " << xi - yi << "i" << endl;
+
+}
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    float x = 5, y = 9;
+    Add(x, y);
+    double f = 3;
+    Add(f, 56);
+    Add(2.0, 2.5, 3.0, 0.1);
+    system("PAUSE");
+    return 0;
+}
---
+++ лабораторная 10/лабораторная 10/лабораторная 10.cpp
@@ -0,0 +1,66 @@
+﻿#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+using namespace std;
+int get_random(void) { return rand() % 100; }
+bool is_even(const int x) { return (x % 2) == 0; }
+
+int main(void)
+{
+    setlocale(0, "rus");
+    srand(static_cast<unsigned int>(time(0)));
+
+    cout << "Введите количество элементов массива: ";
+    int n = 0;
+    cin >> n;
+
+    int* arr = new int[n];
+    int* begin = arr;
+    int* end = arr + n;
+
+  //  Сформировать массив из n элементов с помощью датчика случайных чисел
+    generate(begin, end, get_random);
+    cout << "Исходный массив: ";
+    copy(begin, end, ostream_iterator<int>(cout, " "));
+    cout << "\n\n";
+
+    //   Выполнить удаление указанных элементов из массива.
+    cout << "Введите количество элементов, которые следует удалить: ";
+    int N = 0;
+    cin >> N;
+    cout << "Введите индекс элемента, с которого начать удаление: ";
+    int K = 0;
+    cin >> K;
+    int* begin_del = (K < n) ? begin + K : end;
+    int* end_del = (N + K < n) ? begin_del + N : end;
+    copy(end_del, end, begin_del);
+    end = begin_del;
+    n = distance(begin, end);
+    //)   Вывести полученный результат.
+    cout << "Массив с удалёнными элементами: ";
+    copy(begin, end, ostream_iterator<int>(cout, " "));
+    cout << "\n\n";
+
+    //  Выполнить добавление указанных элементов в массив.
+    cout << "Введите количество элементов, которое нужно добавить: ";
+    cin >> K;
+    n += K;
+    int* new_arr = new int[n];
+    generate(new_arr, new_arr + K, get_random);
+    copy(begin, end, new_arr + K);
+    begin = new_arr;
+    end = new_arr + n;
+    delete[] arr;
+    //   Вывести полученный результат.
+    cout << "Массив с добавленными элементами: ";
+    copy(begin, end, ostream_iterator<int>(cout, " "));
+    cout << "\n\n";
+
+   
+
+    system("pause");
+    return 0;
+}
